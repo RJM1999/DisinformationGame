f21419cf13ed5abfcef726e4bcb410d88e10df1d
RossMaider <rjmaider@dundee.ac.uk>
2020-04-22 17:01:48 +0100

    Added in more assets with images 7 now

--- Disinformation/LevelClass.swift
+++ Disinformation/LevelClass.swift
@@ -32,14 +32,26 @@ class LevelClass
         let twitbotsImg = UIImage(named: "twitterbots.jpeg")
         let tvCampImg = UIImage(named: "TVPPB.jpeg")
         let flyersImg = UIImage(named: "flyers.jpeg")
+        let newSmearImg = UIImage(named: "newssmear.jpeg")
+        let radioImg = UIImage(named: "radio.jpeg")
+        let questionPanelImg = UIImage(named: "questionpanel.jpeg")
+        let rallyImg = UIImage(named: "rally.jpeg")
         
         let twitBot = Asset(name:"Twitter Bots", Desc: "An army of twitter bots that do your bidding",cost: 100,bonus:  10, image: twitbotsImg!)
         let tvCamp = Asset(name:"TV Campaign", Desc: "Party political broadcasts enough to make you change the channel",cost: 50,bonus:  10, image:  tvCampImg!)
         let flyers = Asset(name:"Flyer campaign", Desc: "Post flyers to voters, cheap but often thrown in the bin",cost: 10,bonus:  10, image: flyersImg!)
+        let newssmear = Asset(name:"News smear", Desc: "Enlist national newspaper to start a smear against your adversary",cost: 90,bonus:  10, image: newSmearImg!)
+        let radio = Asset(name:"Radio Show", Desc: "Head on to the air waves for a radio show",cost: 10,bonus:  10, image: radioImg!)
+        let questionPanel = Asset(name:"Question Panel", Desc: "A television show with fellow politicians to answer questions",cost: 10,bonus:  10, image: questionPanelImg!)
+        let rally = Asset(name:"Rally", Desc: "Gather fellow supporters and energize the campaign",cost: 10,bonus:  10, image: rallyImg!)
         
         assetList.append(twitBot)
         assetList.append(tvCamp)
         assetList.append(flyers)
+        assetList.append(newssmear)
+        assetList.append(radio)
+        assetList.append(questionPanel)
+        assetList.append(rally)
     }
     
 }
